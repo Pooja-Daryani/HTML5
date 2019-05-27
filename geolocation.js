1571dacdc73a7bb0d559221072e4cedb0de3feae
Pooja Daryani <pooja.daryani@outlook.com>
2019-05-27 09:17:30 -0700

    HTML5
    This is the final project for my HTML5 class at UC Santa Cruz.

---
+++ geolocation.js
@@ -0,0 +1,134 @@
+// Getting Latitude and Longitude
+window.onload = getMyLocation;
+var watchID;
+var map;
+var options = {
+	enableHighAccuracy:true,
+	timeout:300000,
+	maximumAge:200
+};
+var ourCoords = {
+	latitude: 37.383116,
+	longitude: -121.971931
+};
+
+function getMyLocation() {
+	if(navigator.geolocation) {
+		//navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+		var watchButton = document.getElementById("watch");
+		watchButton.onclick = watchLocation;
+		
+		var clearButton = document.getElementById("clearWatch");
+		clearButton.onclick = clearWatch;
+		
+	}
+	else {
+		alert("Sorry, there is no geolocation support")
+	}
+}
+// Create clearWatch 
+function clearWatch() {
+	if(watchID) {
+		navigator.geolocation.clearWatch(watchID);
+		watchID = null;
+	}
+}
+// Create watchLocation 
+function watchLocation() {
+	watchID = navigator.geolocation.watchPosition(displayLocation, displayError, options)
+}
+
+function displayLocation(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var div = document.getElementById("location");
+	div.innerHTML = "You are at Latitude: "+latitude+", Longitude: "+longitude;
+	div.innerHTML += " (width "+position.coords.accuracy+ " meters accuracy).";
+	
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = document.getElementById("distance");
+	distance.innerHTML = "You are "+km+" km from UCSC Extension";
+	
+	showMap(position.coords);
+	
+}
+
+
+ // Error Message 
+ function displayError(error) {
+ 	var errorTypes = {
+ 		0: "Unknown error", 
+ 		1: "Permission denied by user",
+ 		2: "Position is not available",
+ 		3: "Request timed out"
+ 	};
+ 	var errorMessage = errorTypes[error.code];
+ 	if(error.code==0 || error.code==2) {
+ 		errorMessage = errorMessage +" " +error.message;
+ 	}
+ 	var div = document.getElementById("location");
+ 	div.innerHTML = errorMessage;
+ }
+
+// Calculating Distance 
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+	
+	var Radius = 6371; // radius of the earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + Math.cos(startLatRads)* Math.cos(destLatRads)* Math.cos(startLongRads-destLongRads)) * Radius;
+
+	return distance;
+}
+function degreesToRadians(degrees) {
+	var radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+// Apply or Show Google Map 
+function showMap(coords) {
+	var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+	var mapOptions = {
+		zoom: 18,
+		center: googleLatAndLong,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	
+	var mapDiv = document.getElementById("map");
+	map = new google.maps.Map(mapDiv, mapOptions);
+	
+	// add marker
+	var title = "Your location";
+	var content = "You are here "+coords.latitude +", "+coords.longitude;
+	addMarker(map, googleLatAndLong, title, content);
+}
+
+// Create Marker 
+function addMarker(map, latlong, title, content) {
+	var markerOptions = {
+		position: latlong, 
+		map: map, 
+		title: title,
+		clickable: true
+	};
+	var marker = new google.maps.Marker(markerOptions);
+	
+	var infoWindowOptions = {
+		content: content, 
+		position: latlong
+	};
+	
+	var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+	
+	google.maps.event.addListener(marker, "click", function() {
+		infoWindow.open(map);
+	})  
+}
+
+
+
+
+
+
